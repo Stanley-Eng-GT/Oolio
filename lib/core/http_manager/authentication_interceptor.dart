5ff2c9764701279136b89a33fff5090deb6103f3
Stanley-Eng-GT <stanley.eng.gt@gmail.com>
2025-09-25 17:26:26 +1000

    fixed all issues

--- lib/core/http_manager/authentication_interceptor.dart
+++ lib/core/http_manager/authentication_interceptor.dart
@@ -2,48 +2,17 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
-import 'package:foundation/app_info_manager/app_info_manager.dart';
-import 'package:foundation/local_storage_manager/local_storage_keys.dart';
-import 'package:foundation/local_storage_manager/local_storage_manager.dart';
-import 'package:my_plus/app_constants.dart';
-import 'package:web_services/http_request/request_entity.dart';
-import 'package:web_services/http_request/request_method.dart';
 import 'package:web_services/http_request/web_service_exports.dart';
 import 'package:web_services/http_request/web_services.dart';
-import 'package:web_services/http_request/web_services_constants.dart';
-
-import '../../features/onboarding/screens/loading/models/api_key_response.dart';
-import '../../features/onboarding/screens/login/models/login_response.dart';
-import '../app_constants.dart';
-import '../environment/env.dart';
-import 'end_points.dart';
 import 'http_manager_constants.dart';
 
 class AuthenticationInterceptor extends Interceptor {
   AuthenticationInterceptor({
     required WebServices webServices,
-    required AppInfoManager appInfoManager,
-  })  : _appInfoManager = appInfoManager,
-        _webServices = webServices;
+  })  :_webServices = webServices;
 
   late final WebServices _webServices;
-  final AppInfoManager _appInfoManager;
 
-  @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    if (options.baseUrl == AppConstants.uatBaseUrl ||
-        options.baseUrl == AppConstants.productionBaseUrl) {
-      options.headers.addAll(
-        getInterceptorHeaders(
-          deviceId: _appInfoManager.deviceId,
-          appVersion: _appInfoManager.version,
-          platformName: _appInfoManager.platform,
-        ),
-      );
-    }
-
-    return handler.next(options);
-  }
 
   @visibleForTesting
   Map<String, dynamic> getInterceptorHeaders({
@@ -70,47 +39,6 @@ class AuthenticationInterceptor extends Interceptor {
     return headers;
   }
 
-  @override
-  void onError(DioException err, ErrorInterceptorHandler handler) async {
-    if (err.requestOptions.path == EndPoints.apiKey) {
-      return handler.next(err);
-    } else if (err.requestOptions.path == EndPoints.refresh &&
-        err.response?.statusCode !=
-            WebServicesConstants.apiKeyInvalidStatusCode) {
-      return handler.next(err);
-    } else {
-      switch (err.response?.statusCode) {
-        case WebServicesConstants.apiKeyInvalidStatusCode:
-          // get a new api key
-          final ApiKeyResponse? apiKeyResponse = await getApiKey();
-          if (apiKeyResponse != null) {
-            // assign the new key to header
-            err.requestOptions.headers[HttpManagerConstants.apiKeyHeaderName] =
-                apiKeyResponse.data;
-            // retry the previous api request with new access token
-            return _retryApiRequest(
-              requestOptions: err.requestOptions,
-              handler: handler,
-            );
-          }
-        case WebServicesConstants.tokenExpiredStatusCode:
-          // get a new access token with the current refresh token
-          final LoginResponse? loginResponse = await getTokens();
-          if (loginResponse != null) {
-            // assign the new access token to header
-            err.requestOptions
-                    .headers[HttpManagerConstants.authorizationHeaderName] =
-                HttpManagerConstants.bearer(loginResponse.accessToken);
-            // retry the previous api request with new access token
-            return _retryApiRequest(
-              requestOptions: err.requestOptions,
-              handler: handler,
-            );
-          }
-      }
-      return handler.next(err);
-    }
-  }
 
   Future<void> _retryApiRequest({
     required RequestOptions requestOptions,
@@ -125,69 +53,4 @@ class AuthenticationInterceptor extends Interceptor {
     }
   }
 
-  @visibleForTesting
-  Future<LoginResponse?> getTokens() async {
-    final refreshToken =
-        _localStorageManager.getString(keyName: LocalStorageKeys.refreshToken);
-    try {
-      final responseData = await _webServices.request(
-        requestEntity: RequestEntity(
-          method: RequestMethod.post,
-          path: EndPoints.refresh,
-          data: {
-            HttpManagerConstants.refreshToken: refreshToken,
-          },
-        ),
-      );
-      final LoginResponse loginResponse = LoginResponse.fromJson(responseData);
-      await _localStorageManager.setString(
-          keyName: LocalStorageKeys.refreshToken,
-          value: loginResponse.refreshToken);
-      await _localStorageManager.setString(
-          keyName: LocalStorageKeys.accessToken,
-          value: loginResponse.accessToken);
-      return loginResponse;
-    } on Exception catch (e) {
-      log(e.toString());
-      return null;
-    } on Error catch (e) {
-      log(e.toString());
-      return null;
-    }
-  }
-
-  @visibleForTesting
-  Future<ApiKeyResponse?> getApiKey() async {
-    try {
-      final encodedHeader = base64Encode(
-        utf8.encode('${Env.apiKeyUsername}:${Env.apiKeyPassword}'),
-      );
-      final responseData = await _webServices.request(
-        requestEntity: RequestEntity(
-            method: RequestMethod.get,
-            path: EndPoints.apiKey,
-            queryParameters: {},
-            headers: {
-              HttpManagerConstants.authorizationHeaderName:
-                  HttpManagerConstants.basic(encodedHeader)
-            }),
-      );
-
-      final ApiKeyResponse apiKeyResponse =
-          ApiKeyResponse.fromJson(responseData);
-      final apiKey = apiKeyResponse.data;
-      if (apiKey.isNotEmpty == true) {
-        _localStorageManager.setString(
-            keyName: LocalStorageKeys.apiKey, value: apiKey);
-        return apiKeyResponse;
-      }
-      return null;
-    } on Exception catch (e) {
-      log(e.toString());
-      return null;
-    } on Error catch (e) {
-      log(e.toString());
-      return null;
-    }
-  }
 }
--- lib/core/http_manager/http_manager.dart
+++ lib/core/http_manager/http_manager.dart
@@ -1,8 +1,3 @@
-import 'package:foundation/app_info_manager/app_info_manager.dart';
-import 'package:foundation/local_storage_manager/local_storage_manager.dart';
-import 'package:my_plus/app_constants.dart';
-import 'package:my_plus/core/analytics/analytics_manager.dart';
-import 'package:my_plus/core/auth_manager/auth_manager.dart';
 import 'package:web_services/http_inspector/alice/core/alice_dio_interceptor.dart';
 import 'package:web_services/http_request/request_entity.dart';
 import 'package:web_services/http_request/result.dart';
@@ -10,26 +5,23 @@ import 'package:web_services/http_request/web_service_exports.dart';
 import 'package:web_services/http_request/web_services.dart';
 import 'package:web_services/http_request/web_services_constants.dart';
 
-import '../analytics/analytics_events.dart';
-import '../analytics/analytics_params.dart';
 import '../app_constants.dart';
-import '../environment/dependency_injection.dart';
 import 'authentication_interceptor.dart';
 
 class HttpManager {
   HttpManager({
     WebServices? webServices,
     // AnalyticsManager? analyticsManager,
-    AppInfoManager? appInfoManager,
+    // AppInfoManager? appInfoManager,
     required AliceDioInterceptor aliceDioInterceptor,
-  })  : _authManager = authManager,
+  })  :
         // _analyticsManager = analyticsManager ?? sl<AnalyticsManager>(),
-        _appInfoManager = appInfoManager ?? AppInfoManager(),
+        // _appInfoManager = appInfoManager ?? AppInfoManager(),
         _webServices = webServices ??
             WebServices(baseUrl: AppConstants.productionBaseUrl) {
     _webServices.setInterceptors([
       AuthenticationInterceptor(
-        appInfoManager: _appInfoManager,
+        // appInfoManager: _appInfoManager,
         webServices: _webServices,
       ),
       aliceDioInterceptor,
@@ -41,7 +33,7 @@ class HttpManager {
   }
   late final WebServices _webServices;
   // final AnalyticsManager _analyticsManager;
-  final AppInfoManager _appInfoManager;
+  // final AppInfoManager _appInfoManager;
 
   Future<Result<dynamic, Exception>> request({
     required RequestEntity requestEntity,
@@ -50,17 +42,6 @@ class HttpManager {
       final responseData =
           await _webServices.request(requestEntity: requestEntity);
       return Success(responseData);
-    } on ApiException catch (e) {
-      switch (e.statusCode) {
-        case WebServicesConstants.tokenExpiredStatusCode:
-          (_authManager ?? sl<AuthManager>()).logout();
-        default:
-          break;
-      }
-      // _analyticsManager.logEvent(
-      //     event: AnalyticsEvent.exception,
-      //     params: {AnalyticsParams.errorMessage: e.toString()});
-      return Failure(e);
     } on Exception catch (e) {
       // _analyticsManager.logEvent(
       //     event: AnalyticsEvent.exception,
